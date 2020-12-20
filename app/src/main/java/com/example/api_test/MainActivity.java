3b867b35c01d133aa2775e4e7166f63dea44480a
MuhammadAliGhaffar <alibrohi999@gmail.com>
2020-12-20 19:13:19 +0500

    Getting values JSON Object Element and Getting values JSON Object Array

--- app/src/main/java/com/example/api_test/MainActivity.java
+++ app/src/main/java/com/example/api_test/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.api_test;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,14 +13,20 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     RequestQueue mRequestQueue;
+    RequestQueue mRequestQueueArray;
+    TextView textViewJsonObjectElement,textViewJsonArrayElement;
+    Button btnNextJoke,btnNextJoke2;
 
 
     @Override
@@ -25,24 +34,87 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRequestQueue= Volley.newRequestQueue(this);
+        mRequestQueue = Volley.newRequestQueue(this);
+        mRequestQueueArray = Volley.newRequestQueue(this);
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET
-                , "https://official-joke-api.appspot.com/random_joke"
-                , null, new Response.Listener<JSONObject>() {
+        textViewJsonObjectElement = findViewById(R.id.textViewJsonObjectElement);
+        btnNextJoke = findViewById(R.id.btnNextJoke);
+
+        btnNextJoke.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(JSONObject response) {
-                Log.i("RESPONSE", response + "");
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, error.getMessage().toString(), Toast.LENGTH_SHORT).show();
-                Log.e("RESPONSE ERROR", error.getMessage());
+            public void onClick(View view) {
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET
+                        , "https://official-joke-api.appspot.com/random_joke"
+                        , null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.i("RESPONSE", response + "");
+                        //Getting values JSON Object Element
+                        try {
+                            int id;
+                            String type, setup, punchline;
+                            id = response.getInt("id");
+                            type = response.getString("type");
+                            setup = response.getString("setup");
+                            punchline = response.getString("punchline");
+                            textViewJsonObjectElement.setText("id :" + id + "\n" + "type :" + type + "\n" + "setup :" + setup + "\n" + "puchline :" + punchline);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, error.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                        Log.e("RESPONSE ERROR", error.getMessage());
+                    }
+                });
+
+                mRequestQueue.add(jsonObjectRequest);
             }
         });
 
-        mRequestQueue.add(jsonObjectRequest);
+        textViewJsonArrayElement=findViewById(R.id.textViewJsonArrayElement);
+        btnNextJoke2=findViewById(R.id.btnNextJoke2);
+
+        btnNextJoke2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET
+                        , "https://official-joke-api.appspot.com/random_ten"
+                        , null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        //Getting values JSON Object Array
+                        Log.i("ArrayRESPONSE", response + "");
+                        for (int index = 0; index < response.length(); index++) {
+                            try {
+                                JSONObject jsonObject = response.getJSONObject(index);
+                                //textViewJsonArrayElement.setText(jsonObject.getString("setup")+"\n");
+                                Log.i("TAGG",jsonObject.getString("setup")+"\n");
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i("ArrayRESPONSEError", error.getMessage());
+
+                    }
+                });
+
+
+                mRequestQueueArray.add(jsonArrayRequest);
+
+            }
+        });
+
+
+
 
     }
 }
